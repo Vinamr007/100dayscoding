a21888ec853f45bee8f7e6038d1ab1cafbbe0607
Vinamr Naik <vinamrnaik206@gmail.com>
2025-11-01 10:53:33 +0530

    Update files

---
+++ day2/datatypee.java
@@ -0,0 +1,19 @@
+public class datatypee{
+    public static void main(String[] args){
+
+        int age=90;
+        float pi=3.14f;
+        double largeDecimal=3.141592653589793;  
+        char grade='A';
+        String name="John Doe";
+
+        System.out.println("Age: " + age);
+        System.out.println("Pi: " + pi);
+
+        System.out.println("Large Decimal: " + largeDecimal);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    
+        
+    }
+}
