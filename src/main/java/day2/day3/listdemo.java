25e367611f65d7a919af5c9247198c888cfde374
Vinamr Naik <vinamrnaik206@gmail.com>
2025-11-04 10:49:20 +0530

    Restructure: move sources to src/main/java; add .gitignore; remove build and IDE artifacts

---
+++ src/main/java/day2/day3/listdemo.java
@@ -0,0 +1,18 @@
+package day2.day3;
+import java.util.List;
+import java.util.ArrayList;
+
+public class listdemo { 
+    public static void main(String[] args) {
+        List<String> users = new ArrayList<>();
+        users.add("vinu");
+        users.add("sai");
+
+           System.out.println("all user");
+    for (String u : users) {
+        System.out.println(u);
+    }
+}
+    }
+    
+
